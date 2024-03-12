4abf2e87617002d0d6df3a07ad5f8344921d1e95
Vovazast1 <volodyazastavny@gmail.com>
2024-03-12 21:12:35 +0200

    TABTAB-20-Connect-Spring-Boot-to-our-project

---
+++ backend/src/test/java/com/example/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.example;
+
+// import org.junit.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class DemoApplicationTests {
+
+// @Test
+// void contextLoads() {
+// }
+
+// }
