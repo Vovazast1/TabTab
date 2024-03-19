577335c2e6494f25219f7fd619b67f82e43e0cc1
Vovazast1 <volodyazastavny@gmail.com>
2024-03-19 10:40:14 +0200

    create button function

--- frontend/src/app/pages/pages.ts
+++ frontend/src/app/pages/pages.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Loader } from '../loader/loader';
-import { FirstPage } from '../first-page/first-page';
+import { LoginPage } from '../login-page/login-page';
+import { RegisterPage } from '../register-page/register-page';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.html',
   styleUrls: ['./pages.scss'],
   standalone: true,
-  imports: [Loader, FirstPage, IonicModule],
+  imports: [Loader, LoginPage, RegisterPage, IonicModule],
 })
 export class Pages {
   LoaderRoot: any = Loader;
-  FirstPageRoot: any = FirstPage;
+  LoginPageRoot: any = LoginPage;
+  RegisterPageRoot: any = RegisterPage;
 
   constructor() {}
 
