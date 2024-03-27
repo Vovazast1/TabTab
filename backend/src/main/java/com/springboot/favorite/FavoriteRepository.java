bd871207e4cc146445f1745ecba801de6c8af1d5
Vovazast1 <volodyazastavny@gmail.com>
2024-03-27 11:22:16 +0200

    login create account requests

---
+++ backend/src/main/java/com/springboot/favorite/FavoriteRepository.java
@@ -0,0 +1,7 @@
+//package com.springboot.favorite;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+//
+//}
