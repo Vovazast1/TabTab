b4152988b8dd08adaba8f374db9d12dde1ba8a73
Chakloon <pirklviktor@gmail.com>
2024-03-28 13:33:37 +0200

    activity page

---
+++ frontend/src/app/activity/activity.page.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivityPage } from './activity.page';
+
+describe('ActivityPage', () => {
+  let component: ActivityPage;
+  let fixture: ComponentFixture<ActivityPage>;
+
+  //async after      beforeEach(async
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActivityPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/activity/activity.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity',
+  templateUrl: './activity.page.html',
+  styleUrls: ['./activity.page.scss'],
+})
+export class ActivityPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
