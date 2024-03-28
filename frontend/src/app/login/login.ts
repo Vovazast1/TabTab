626de73ce6b18454e9f4396213aa3fb1804eecd3
Chakloon <pirklviktor@gmail.com>
2024-03-28 13:29:19 +0200

    login function

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginPageForm } from './login.form';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.html',
-  styleUrls: ['login.scss']
+  styleUrls: ['login.scss'],
 })
-export class LoginPage  {
+export class LoginPage implements OnInit {
+  form: FormGroup | undefined;
 
-  constructor() {}
-  
-  public login() {
-    console.log("Hello Hell");
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+  ) {}
+
+  login() {
+    this.router.navigate(['activity']);
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
+
+  ngOnInit() {
+    this.form = new LoginPageForm(this.formBuilder).createForm();
   }
 }
-
