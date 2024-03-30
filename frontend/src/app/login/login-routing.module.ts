1fc6f82f2fc279a9a39346a4c77f2186f3bd33ec
Chakloon <pirklviktor@gmail.com>
2024-03-30 18:23:39 +0200

    login page valid input

--- frontend/src/app/login/login-routing.module.ts
+++ frontend/src/app/login/login-routing.module.ts
@@ -6,11 +6,11 @@ const routes: Routes = [
   {
     path: '',
     component: LoginPage,
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class LoginPageRoutingModule {}
