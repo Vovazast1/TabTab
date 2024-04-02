ac664f60c13f7ddee55765cb85207574106e7dc0
Nazar Korop <nazar.koropetskyi.pm.2022@lpnu.ua>
2024-04-02 13:53:13 +0300

    jwt tokens

--- backend/src/main/java/com/springboot/security/JwtTokenProvider.java
+++ backend/src/main/java/com/springboot/security/JwtTokenProvider.java
@@ -1,23 +1,26 @@
 package com.springboot.security;
 
 import com.springboot.exception.APIException;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Component
-public class JwtTokenProvider {
+@Data
+public class JwtTokenProvider    {
 
     @Value("${app.jwt-secret}")
     private String jwtSecret;
@@ -26,37 +29,67 @@ public class JwtTokenProvider {
     private long jwtExpirationDate;
 
     // generate JWT token
-    public String generateToken(Authentication authentication){
-
-        String username = authentication.getName();
-
-        Date currentDate = new Date();
-
-        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
-
-        String token = Jwts.builder()
-                .subject(username)
-                .issuedAt(new Date())
-                .expiration(expireDate)
-                .signWith(key())
-                .compact();
-
-        return token;
-    }
+//    public String generateToken(Authentication authentication){
+//
+//        String username = authentication.getName();
+//
+//        Date currentDate = new Date();
+//
+//        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+//
+//        String token = Jwts.builder()
+//                .subject(username)
+//                .issuedAt(new Date())
+//                .expiration(expireDate)
+//                .signWith(key())
+//                .compact();
+//
+//        return token;
+//    }
 
     private Key key(){
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
-    // get username from JWT token
-    public String getUsername(String token){
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 
-        return Jwts.parser()
-                .verifyWith((SecretKey) key())
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(key())
                 .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .getSubject();
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String GenerateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*1))
+                .signWith(key(), SignatureAlgorithm.HS256).compact();
     }
 
     // validate JWT token
@@ -77,4 +110,18 @@ public class JwtTokenProvider {
             throw new APIException(HttpStatus.BAD_REQUEST, "Jwt claims string is null or empty");
         }
     }
+
+
+    // get username from JWT token
+//    public String getUsername(String token){
+//        return Jwts.parser()
+//                .verifyWith((SecretKey) key())
+//                .build()
+//                .parseSignedClaims(token)
+//                .getPayload()
+//                .getSubject();
+//    }
+
+    // validate JWT token
+
 }
--- backend/src/main/java/com/springboot/service/AuthService.java
+++ backend/src/main/java/com/springboot/service/AuthService.java
@@ -2,7 +2,9 @@ package com.springboot.service;
 
 import com.springboot.payload.LoginDto;
 import com.springboot.payload.RegisterDto;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface AuthService {
     String login(LoginDto loginDto);
 
