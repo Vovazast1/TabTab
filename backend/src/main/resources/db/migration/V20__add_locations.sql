e4ee087799fa3dc8588efc6be5178e92f5309aac
Nazar Korop <nazar.koropetskyi.pm.2022@lpnu.ua>
2024-04-20 12:58:45 +0300

    Added locations to location table

--- backend/src/main/resources/db/migration/V20__add_locations.sql
+++ backend/src/main/resources/db/migration/V20__add_locations.sql
@@ -1,8 +1,41 @@
 INSERT INTO tabtab.location
-VALUES('0', 'Гетьмана Мохняка 18а','Спортивна','Баскетбол', 49.4431, 24.4361);
+VALUES('0', 'Oleksandra Dovzhenka 3','Sport','Football', 49.799090311014425, 24.052896837753053);
 
 INSERT INTO tabtab.location
-VALUES('0', 'Цукрова 22','Інтелектуальна','Квест кімната', 49.6431, 24.2361);
+VALUES('0', 'Stavova 7B','Intelligence','Quest room', 49.85491255522468, 24.023564244422936);
 
 INSERT INTO tabtab.location
-VALUES('0', 'Сирна 13','Спортивна','Футбол', 49.2431, 24.6361);
+VALUES('0', 'Kastelivka 24','Sport','Football', 49.83089918052624, 24.013040908518633);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Oleksandra Dovzhenka 13','Sport','Football', 49.8005538375271, 24.056341331466506);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Zubrivska 30','Sport','Football', 49.79815192658522, 24.060556091247623);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Hutorivka 26','Sport','Gym', 49.800476497989074, 24.050889519099858);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Na Valakh Square','Sport','Basketball', 49.84359849318463, 24.0353900535317);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Chornomorska 12','Sport','Football', 49.84569111092758, 24.03098191114354);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Svobody avenue','Intelligence','Chess', 49.842459809240594, 24.02721045358292);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Park of Culture','Intelligence','Park', 49.83003814733029, 24.02213208521727);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Stryiskiy park','Intelligence','Park', 49.823130802690976, 24.024948034319696);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Studentskiy park','Intelligence','Park', 49.8283960848387, 24.013215942817983);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Stryyska 45A','Sport','Gym', 49.80315580350711, 24.020297526379757);
+
+INSERT INTO tabtab.location
+VALUES('0', 'Hutorivka 24','Intelligence','Library', 49.800273803111395, 24.05128461392078);
