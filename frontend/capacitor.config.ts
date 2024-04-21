40a4c2198a0e14ce3d653a95d9d438dfade6377c
Chakloon <pirklviktor@gmail.com>
2024-04-21 12:18:49 +0300

    try11

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.mycompany.tabtab',
   appName: 'TabTab',
   webDir: 'www',
   server: {
