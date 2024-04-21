c8df1eec5e91be775976e65c9f61ac732eece8ee
Chakloon <pirklviktor@gmail.com>
2024-04-21 11:16:04 +0300

    try 1

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'TabTab',
   appName: 'TabTab',
   webDir: 'www',
   server: {
