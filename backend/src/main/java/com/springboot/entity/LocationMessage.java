5e6eca0472a8f32edbbc6f9e470d965dc23891e7
korop42 <nazgo098@gmail.com>
2024-04-25 12:10:44 +0300

    Location chat

---
+++ backend/src/main/java/com/springboot/entity/LocationMessage.java
@@ -0,0 +1,35 @@
+package com.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Date;
+
+@Entity
+@Table(name = "Location_Message")
+@Getter
+@Setter
+public class LocationMessage {
+
+    @Id
+    @Column(name = "LocationMessageID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long locationMessageID;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "UserID", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "LocationID", nullable = false)
+    private Location location;
+
+    @Column(name = "Message")
+    private String message;
+
+    @Column(name = "Timestamp")
+    private Date timestamp;
+}
---
+++ backend/src/main/java/com/springboot/payload/MessageType.java
@@ -0,0 +1,7 @@
+package com.springboot.payload;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
