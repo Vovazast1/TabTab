cbe75093b18fdf3162c4fd99d1bf4e9a824d9103
kopystianska <viktoriiakopystianska@gmail.com>
2024-05-06 21:26:46 +0300

    profile html+css

--- frontend/src/app/profile/profile.page.ts
+++ frontend/src/app/profile/profile.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivityType } from '../data';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  currentActivity?: ActivityType;
+  map!: L.Map;
+  public l: string = '';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
+
+  
+  async goToLocations() {
+    const newActivity = this.currentActivity === ActivityType.Sport ? ActivityType.Intelligence : ActivityType.Sport;
+    if (this.map) {
+          this.map.remove();
+        }
+    await this.router.navigate(['pages/locations', newActivity]);
+    window.location.reload();
+  }
+  
   ngOnInit() {
   }
 
 }
+
