0390e700401e43b7ce43eae0528f3bbe5cbc92a7
korop42 <nazgo098@gmail.com>
2024-05-12 10:30:08 +0300

    database fix

--- backend/src/main/resources/db/migration/V31_rename_column_location_message.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE tabtab.location_message
-CHANGE LocationMessageID location_messageid int auto_increment NOT NULL
-
-ALTER TABLE tabtab.location_message
-ADD message_type varchar(100) DEFAULT('CLIENT');
